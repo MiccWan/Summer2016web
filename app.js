eb3d0e7ccbe7c3a0d411a5b86a9d8c906f309b83
taWeiTu <tu.da.wei@gmail.com>
2016-07-09 22:19:00 +0800

    nothing

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride("_method"));
 app.use(flash());
-// seedDB();
+//seedDB();
 
 app.use(require('express-session')({
 	secret: "Jizz",
